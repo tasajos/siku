2632b2362d42442f54a89ab5c95569b9e16665d1
cazcarraga <cazcarraga@hipermaxi.com>
2024-10-25 14:24:28 -0400

    actualizar_encabezado

--- siku/src/Components/Layout/Encabezado.js
+++ siku/src/Components/Layout/Encabezado.js
@@ -1,9 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 const Encabezado = ({ onSearch }) => {
   const [busqueda, setBusqueda] = useState('');
+  const [fechaHora, setFechaHora] = useState('');
+
+  // Función para actualizar la fecha y hora
+  useEffect(() => {
+    const actualizarFechaHora = () => {
+      const fechaActual = new Date();
+      const opcionesFecha = { year: 'numeric', month: 'long', day: 'numeric' };
+      const opcionesHora = { hour: '2-digit', minute: '2-digit', second: '2-digit' };
+
+      const fechaFormateada = fechaActual.toLocaleDateString('es-BO', opcionesFecha);
+      const horaFormateada = fechaActual.toLocaleTimeString('es-BO', opcionesHora);
+
+      setFechaHora(`${fechaFormateada} ${horaFormateada}`);
+    };
+
+    // Actualiza la fecha y hora cada segundo
+    const intervalId = setInterval(actualizarFechaHora, 1000);
+
+    return () => clearInterval(intervalId); // Limpia el intervalo cuando el componente se desmonta
+  }, []);
 
   const manejarCambio = (event) => {
     const valorBusqueda = event.target.value;
@@ -13,7 +33,7 @@ const Encabezado = ({ onSearch }) => {
 
   return (
     <nav className="navbar navbar-light bg-light p-3">
-      <div className="navbar-brand">POS</div>
+      <div className="navbar-brand">POS - SIKU - Chakuy</div>
 
       <div className="input-group w-50">
         <input
@@ -28,6 +48,11 @@ const Encabezado = ({ onSearch }) => {
             <FontAwesomeIcon icon={faSearch} />
           </span>
         </div>
+      </div>
+
+      {/* Agregar fecha y hora al encabezado */}
+      <div className="ml-auto">
+        <span>{fechaHora}</span>
       </div>
     </nav>
   );
