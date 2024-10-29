09c611a273306da295dcba0f47794111f8f4068d
tasajos <tasajos@gmail.com>
2024-10-29 19:55:44 -0400

    actualizar usuario

--- siku/src/Components/Layout/BarraLateral.js
+++ siku/src/Components/Layout/BarraLateral.js
@@ -1,14 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faHome, faUtensils, faMoneyBillAlt, faClipboardList, faCog, faPlusCircle, faSignOutAlt, faChevronDown, faHamburger, faPizzaSlice } from '@fortawesome/free-solid-svg-icons';
+import {
+  faHome, faUtensils, faMoneyBillAlt, faClipboardList, faCog, faPlusCircle, faSignOutAlt, faChevronDown,
+  faHamburger, faPizzaSlice
+} from '@fortawesome/free-solid-svg-icons';
 import { getAuth, signOut } from 'firebase/auth';
+import { database } from '../../firebase'; // Firebase para obtener datos del usuario
+import { ref, onValue } from 'firebase/database';
 import './BarraLateral.css';
 import logo from '../../assets/logo_cha.png';
 
 const BarraLateral = () => {
   const [submenuVisible, setSubmenuVisible] = useState(false);
+  const [userRole, setUserRole] = useState('');
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const auth = getAuth();
+    const user = auth.currentUser;
+
+    if (user) {
+      const userRoleRef = ref(database, `usuarios/${user.uid}/rol`); // Cambiado a 'usuarios'
+      onValue(userRoleRef, (snapshot) => {
+        const role = snapshot.val();
+        setUserRole(role);
+      });
+    }
+  }, []);
 
   const handleLogout = () => {
     const auth = getAuth();
@@ -25,6 +44,10 @@ const BarraLateral = () => {
     setSubmenuVisible(!submenuVisible);
   };
 
+  const isAdminOrManager = userRole === 'Administrador' || userRole === 'Gerente';
+  const isCashier = userRole === 'Cajero';
+  const isSupervisor = userRole === 'Supervisor';
+
   return (
     <aside className="barra-lateral bg-danger text-light">
       <div className="logo text-center mt-3">
@@ -37,53 +60,67 @@ const BarraLateral = () => {
             Inicio
           </Link>
         </li>
-        <li>
-          <Link to="/menu" className="nav-link text-light">
-            <FontAwesomeIcon icon={faUtensils} className="mr-2" />
-            Menú
-          </Link>
-        </li>
-        <li>
-          <Link to="/pagos" className="nav-link text-light">
-            <FontAwesomeIcon icon={faMoneyBillAlt} className="mr-2" />
-            Pago
-          </Link>
-        </li>
+
+        {(isAdminOrManager || isCashier || isSupervisor) && (
+          <li>
+            <Link to="/menu" className="nav-link text-light">
+              <FontAwesomeIcon icon={faUtensils} className="mr-2" />
+              Menú
+            </Link>
+          </li>
+        )}
+
+        {(isAdminOrManager || isCashier) && (
+          <li>
+            <Link to="/pagos" className="nav-link text-light">
+              <FontAwesomeIcon icon={faMoneyBillAlt} className="mr-2" />
+              Pago
+            </Link>
+          </li>
+        )}
+
         <li>
           <Link to="/pedidos" className="nav-link text-light">
             <FontAwesomeIcon icon={faClipboardList} className="mr-2" />
             Pedidos
           </Link>
         </li>
-        <li>
-          <Link to="/configuracion" className="nav-link text-light">
-            <FontAwesomeIcon icon={faCog} className="mr-2" />
-            Configuración
-          </Link>
-        </li>
-        <li>
-          <button onClick={toggleSubmenu} className="nav-link text-light btn btn-link d-flex align-items-center">
-            <FontAwesomeIcon icon={faPlusCircle} className="mr-2" />
-            Configuracion Menú
-            <FontAwesomeIcon icon={faChevronDown} className={`ml-auto ${submenuVisible ? 'rotate-icon' : ''}`} />
-          </button>
-          {submenuVisible && (
-            <ul className="submenu">
-              <li>
-                <Link to="/agregar-menu" className="submenu-link">
-                  <FontAwesomeIcon icon={faHamburger} className="mr-2" />
-                  Nuevo Producto
-                </Link>
-              </li>
-              <li>
-                <Link to="/nueva-categoria" className="submenu-link">
-                  <FontAwesomeIcon icon={faPizzaSlice} className="mr-2" />
-                  Nueva Categoría
-                </Link>
-              </li>
-            </ul>
-          )}
-        </li>
+
+        {isAdminOrManager && (
+          <li>
+            <Link to="/configuracion" className="nav-link text-light">
+              <FontAwesomeIcon icon={faCog} className="mr-2" />
+              Configuración
+            </Link>
+          </li>
+        )}
+
+        {(isAdminOrManager || isSupervisor) && (
+          <li>
+            <button onClick={toggleSubmenu} className="nav-link text-light btn btn-link d-flex align-items-center">
+              <FontAwesomeIcon icon={faPlusCircle} className="mr-2" />
+              Configuración Menú
+              <FontAwesomeIcon icon={faChevronDown} className={`ml-auto ${submenuVisible ? 'rotate-icon' : ''}`} />
+            </button>
+            {submenuVisible && (
+              <ul className="submenu">
+                <li>
+                  <Link to="/agregar-menu" className="submenu-link">
+                    <FontAwesomeIcon icon={faHamburger} className="mr-2" />
+                    Nuevo Producto
+                  </Link>
+                </li>
+                <li>
+                  <Link to="/nueva-categoria" className="submenu-link">
+                    <FontAwesomeIcon icon={faPizzaSlice} className="mr-2" />
+                    Nueva Categoría
+                  </Link>
+                </li>
+              </ul>
+            )}
+          </li>
+        )}
+
         <li>
           <button onClick={handleLogout} className="nav-link text-light btn btn-link">
             <FontAwesomeIcon icon={faSignOutAlt} className="mr-2" />
